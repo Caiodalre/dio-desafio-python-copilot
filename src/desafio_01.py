3984c35e5b3468fc436c7ced806510f9f71a7147
Caio Dal Ré <116048540+Caiodalre@users.noreply.github.com>
2026-05-09 23:48:27 -0300

    Delete src directory

--- src/desafio_01.py
+++
@@ -1,7 +0,0 @@
-repository_url = input().strip()
-
-git_command = "git clone"
-
-full_command = f"{git_command} {repository_url}"
-
-print(full_command)
--- src/src/desafio_02.py
+++
@@ -1,7 +0,0 @@
-files_to_add = input().strip()
-
-git_command = "git add"
-
-full_command = f"{git_command} {files_to_add}"
-
-print(full_command)
